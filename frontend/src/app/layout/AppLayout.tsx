cdb5e2f5d42d1c04624fafaed3e9a423d21bacde
Dmitry Slugin <ethernalfoxd@gmail.com>
2024-08-31 21:46:39 +0300

    moved navbar

--- frontend/src/app/layout/AppLayout.tsx
+++ frontend/src/app/layout/AppLayout.tsx
@@ -1,5 +1,5 @@
 import { Header } from "@shared/ui/Header";
-import { Navbar } from "@shared/ui/Navbar";
+import { Navbar } from "@widgets/Navbar";
 import { Outlet } from "react-router-dom";
 import styles from "./AppLayout.module.css";
 
